b5165f676147fdca81f8df88e4a0318746462f77
mosescode1 <emosescode@gmail.com>
2024-08-29 13:48:53 +0100

    reduceed function length

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,10 +7,6 @@ def filter_datum(fields: list, redaction: str, message: str, separator: str):
     """Filter_datum for filtering protected data"""
 
     for field in fields:
-        # Construct a regex pattern to match 'field=value' format
-        pattern = rf'{field}=.*?(?={separator}|$)'
-        print(pattern)
-        # Replace the field value with the redaction string
-        message = re.sub(pattern, f'{field}={redaction}', message)
-
+        message = re.sub(rf'{field}=.*?(?={separator}|$)',
+                         f'{field}={redaction}', message)
     return message
