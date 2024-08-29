5e0508ce752f06c15f6b54777b900010c9025e4e
mosescode1 <emosescode@gmail.com>
2024-08-29 15:00:37 +0100

    password hashing and checking

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,12 +4,12 @@ import bcrypt
 from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """Hassing password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode("utf-8"), salt)
 
 
-def is_valid(hashed_password: bytes, password) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if password is valid"""
     return bcrypt.checkpw(password.encode(), hashed_password)
