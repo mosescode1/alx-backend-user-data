293083d6119f3e74794bd7e3f75be9ea237fff55
mosescode1 <emosescode@gmail.com>
2024-08-29 13:44:11 +0100

    A fillter funtion that hides user sensitive data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flitering user data obfuscating them"""
+import re
+
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str):
+    """Filter_datum for filtering protected data"""
+
+    for field in fields:
+        # Construct a regex pattern to match 'field=value' format
+        pattern = rf'{field}=.*?(?={separator}|$)'
+        print(pattern)
+        # Replace the field value with the redaction string
+        message = re.sub(pattern, f'{field}={redaction}', message)
+
+    return message
