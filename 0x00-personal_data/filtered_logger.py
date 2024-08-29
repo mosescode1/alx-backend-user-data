636bc0cce724b73c93c01a05e70ee93963f6e184
mosescode1 <emosescode@gmail.com>
2024-08-29 14:24:18 +0100

    changes to the filltering function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 """Flitering user data obfuscating them"""
+import logging
 import re
 from typing import List
 
 
-def filter_datum(fields: List, redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Filter_datum for filtering protected data"""
     for field in fields:
         message = re.sub(rf'{field}=.*?(?={separator}|$)',
                          f'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
