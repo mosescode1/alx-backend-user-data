59d26b89f51d2041aae1d67541f0d6740e631c0d
mosescode1 <emosescode@gmail.com>
2024-08-29 13:53:09 +0100

    initial working space

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flitering user data obfuscating them"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str, message: str, separator: str):
+    """Filter_datum for filtering protected data"""
+    for field in fields:
+        message = re.sub(rf'{field}=.*?(?={separator}|$)',
+                         f'{field}={redaction}', message)
+    return message
