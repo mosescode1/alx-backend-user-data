9e2bbc2bc5eee295bfd7206ef5701d6dfb28f303
mosescode1 <emosescode@gmail.com>
2024-09-02 13:37:40 +0100

    required authentication

--- 0x01-Basic_authentication/api/auth/auth.py
+++ 0x01-Basic_authentication/api/auth/auth.py
@@ -6,7 +6,21 @@ from typing import List, TypeVar
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method for authenticating"""
-        return False
+
+        if not path:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        # Check if the normalized path is in the list of excluded paths
+        if normalized_path in excluded_paths:
+            return False
+
+        # If path is not in excluded_paths, return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method for request headers"""
