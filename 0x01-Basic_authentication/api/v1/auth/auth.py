96a26ee24f8786aad595ce6b3493e9ce0c06ee1a
mosescode1 <emosescode@gmail.com>
2024-09-02 14:35:50 +0100

    BasicAuth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+"""Authentication class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method for authenticating"""
+
+        if not path:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        # Check if the normalized path is in the list of excluded paths
+        if normalized_path in excluded_paths:
+            return False
+
+        # If path is not in excluded_paths, return True
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Method for request headers"""
+
+        if request is None:
+            return None
+
+        if request.headers.get("Authorization") is None:
+            return None
+
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retuning the authenticated user"""
+        return None
+
+
+class BasicAuth(Auth):
+    pass
