4bbe560c01584db610a27297258081da2387ff09
mosescode1 <emosescode@gmail.com>
2024-09-03 19:33:09 +0100

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """Basic  Authenication"""
-from .auth import Auth
+from auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic authentication models"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Base64 part"""
+        if not authorization_header or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
+
+
+a = BasicAuth()
+
+print(a.extract_base64_authorization_header(None))
+print(a.extract_base64_authorization_header(89))
+print(a.extract_base64_authorization_header("Holberton School"))
+print(a.extract_base64_authorization_header("Basic Holberton"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.extract_base64_authorization_header("Basic1234"))
