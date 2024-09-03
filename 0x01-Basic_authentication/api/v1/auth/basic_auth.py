0e220d41bf968d789472b0301486c67a05352e11
mosescode1 <emosescode@gmail.com>
2024-09-03 20:06:47 +0100

    all done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """Basic  Authenication"""
-from auth import Auth
+from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic authentication models"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Base64 part for basic auth"""
-        if not authorization_header or not isinstance(authorization_header, str):
-            return None
-        if not authorization_header.startswith("Basic "):
+        """Returns Base64 part for basic auth"""
+        if (
+            not authorization_header
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith("Basic ")
+        ):
             return None
 
         return authorization_header[6:]
