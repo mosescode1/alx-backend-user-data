2c998c67016feae389e8b427595346963cc3d91c
mosescode1 <emosescode@gmail.com>
2024-09-03 18:58:31 +0100

    sone

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,8 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """Authenticaltion class"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method for authenticating"""
 
