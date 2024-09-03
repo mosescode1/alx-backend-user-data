9c7613215d30eb156eba3c752844ecc94ec0e7a6
mosescode1 <emosescode@gmail.com>
2024-09-03 20:28:11 +0100

    decoding byte string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """Basic  Authenication"""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic authentication models"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """Returns Base64 part for basic auth"""
+
         if (
             not authorization_header
             or not isinstance(authorization_header, str)
@@ -16,3 +19,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """DEcode"""
+        if base64_authorization_header is None:
+            return None
+
+        try:
+            string = base64.b64decode(
+                base64_authorization_header).decode("utf-8")
+            return string
+        except Exception as e:
+            return None
