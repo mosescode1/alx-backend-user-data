07c2c590f73d22ce1ef78679bd0efd4296e6be38
mosescode1 <emosescode@gmail.com>
2024-09-03 19:34:15 +0100

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,14 +14,3 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
-
-
-a = BasicAuth()
-
-print(a.extract_base64_authorization_header(None))
-print(a.extract_base64_authorization_header(89))
-print(a.extract_base64_authorization_header("Holberton School"))
-print(a.extract_base64_authorization_header("Basic Holberton"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
-print(a.extract_base64_authorization_header("Basic1234"))
