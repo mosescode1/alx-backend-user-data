a2248ea7bd8cd7ccc7cdc1c6f439469594987850
mosescode1 <emosescode@gmail.com>
2024-09-10 16:38:24 +0100

    done

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,12 +14,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
-
-    # def __init__(self, email: str, hashed_password: ByteString, session_id=None, reset_token=None):
-    #     if not session_id is None:
-    #         self.session_id = session_id
-    #     if not reset_token is None:
-    #         self.reset_token = reset_token
-
-    #     self.email = email
-    #     self.hashed_password = hashed_password
