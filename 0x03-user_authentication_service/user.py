506478f2f1da173fd827dcba88055ce10f74ec61
mosescode1 <emosescode@gmail.com>
2024-09-10 16:31:34 +0100

    redo

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,16 @@ class User(Base):
     """User Model"""
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=False)
-    reset_token = Column(String, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String((250)), nullable=False)
+    session_id = Column(String((250)), nullable=True)
+    reset_token = Column(String((250)), nullable=True)
+
+    def __init__(self, email, hashed_password, session_id=None, reset_token=None):
+        if not session_id is None:
+            self.session_id = session_id
+        if not reset_token is None:
+            self.reset_token = reset_token
+
+        self.email = email
+        self.hashed_password = hashed_password
